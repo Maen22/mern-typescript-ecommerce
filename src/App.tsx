500eb7948df9cf805d8e7b4f0fed08746773566f
Maen Ibreigheith <m3n.ibregeth@gmail.com>
2021-01-31 21:54:47 +0200

    basic layout with navbar added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
-
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Container } from "react-bootstrap";
 
 function App() {
-    return (
-        <div>
-            Hello E Commerce!
-        </div>
-    );
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to Bazaar</h1>
+        </Container>
+      </main>
+
+      <Footer />
+    </>
+  );
 }
 
 export default App;
