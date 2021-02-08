4456c4154e8f552d24f32d01af484f84d1ca59c2
Maen22 <m3n.ibregeth@gmail.com>
2021-02-08 21:45:32 +0200

    setup nodejs and express with typescript

---
+++ server/server.ts
@@ -0,0 +1,20 @@
+import express, { Request, Response } from 'express'
+import products from './data/products'
+
+const app = express()
+
+app.get('/', (_, res: Response) => {
+  res.send('API is running...')
+})
+app.get('/api/products', (_, res: Response) => {
+  res.json(products)
+})
+app.get('/api/products/:id', (req: Request, res: Response) => {
+  let id = req.params.id
+  let product = products.find((p) => p._id === id)
+  res.json(product)
+})
+
+app.listen(5000, () => {
+  console.log('Server running on port 5000')
+})
